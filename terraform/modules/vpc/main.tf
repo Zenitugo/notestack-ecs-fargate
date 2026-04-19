a3ba71fd2f27ebd54c1b3227616bf46fbb84703c
zenitugo <ugochiukaegbu19@gmail.com>
2026-04-19 12:07:01 +0100

    create configuration to associate private subnet 1 to private route tables

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -153,3 +153,10 @@ resource "aws_route_table_association" "public_subnet_2_assoc" {
   subnet_id = aws_subnet.public_subnet_2.id
   route_table_id = aws_route_table.public_rt.id
 }
+
+
+# Associate private subnet 1 with private route table
+resource "aws_route_table_association" "private_subnet_1_assoc" {
+  subnet_id = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private_rt_1.id
+}
