90027bb75eb6d779adfc1af607d40c33e6bb2efb
zenitugo <ugochiukaegbu19@gmail.com>
2026-04-19 11:18:44 +0100

    configuration to output private subnet 1 id

--- terraform/modules/vpc/output.tf
+++ terraform/modules/vpc/output.tf
@@ -12,3 +12,8 @@ output "public_subnet_1_id" {
 output "public_subnet_2_id" {
   value = aws_subnet.public_subnet_2.id
 }
+
+# Output the ID of the private subnet 1
+output "private_subnet_1_id" {
+  value = aws_subnet.private_subnet_1.id    
+}
