35cba6f675fe108c927e75a1368e08ee9a18ca71
zenitugo <ugochiukaegbu19@gmail.com>
2026-04-19 13:03:04 +0100

    configuration for notestack database sg declared

--- terraform/modules/sg/main.tf
+++ terraform/modules/sg/main.tf
@@ -56,3 +56,22 @@ resource "aws_security_group" "ecs_sg_backend" {
     }
 }
 
+
+# Create Security Group for RDS
+resource "aws_security_group" "rds_sg" {
+  name        = "${var.project_name}-rds-sg"
+  description = "Security group for RDS"
+  vpc_id      = aws_vpc.vpc.id
+    ingress {
+        from_port   = 5432
+        to_port     = 5432
+        protocol    = "tcp"         
+        security_groups = [aws_security_group.ecs_sg_backend.id]
+    }
+    egress {
+        from_port   = 0         
+        to_port     = 0       
+        protocol    = "-1"        
+        cidr_blocks = ["0.0.0.0/0"] 
+    }
+}
