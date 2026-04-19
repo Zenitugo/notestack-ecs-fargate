1e1bb08a9a7287818180108537a60092b2e862dc
zenitugo <ugochiukaegbu19@gmail.com>
2026-04-19 13:14:38 +0100

    output resource for ecs backend sg id declared

--- terraform/modules/sg/output.tf
+++ terraform/modules/sg/output.tf
@@ -8,3 +8,9 @@ output "alb_sg_id" {
 output "ecs_sg_frontend_id" {
   value = aws_security_group.ecs_sg_frontend.id
 }
+
+
+# output ecs backend security group ID
+output "ecs_sg_backend_id" {
+  value = aws_security_group.ecs_sg_backend.id
+}
