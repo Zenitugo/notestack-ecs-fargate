cd6e017134ad830a3de4dded307cdfee2f2db822
zenitugo <ugochiukaegbu19@gmail.com>
2026-04-19 13:02:11 +0100

    configuration for notestack frontend sg declared

--- terraform/modules/sg/main.tf
+++ terraform/modules/sg/main.tf
@@ -17,4 +17,23 @@ resource "aws_security_group" "alb_sg" {
     }
 }
 
+# Create Security Group for ECS tasks Frontend
+resource "aws_security_group" "ecs_sg_frontend" {
+  name        = "${var.project_name}-ecs-sg-frontend"
+  description = "Security group for ECS tasks - Frontend"
+  vpc_id      = aws_vpc.vpc.id
+    ingress {
+        from_port   = 3000
+        to_port     = 3000
+        protocol    = "tcp"
+        security_groups = [aws_security_group.alb_sg.id]
+    }
+    egress {
+        from_port   = 0
+        to_port     = 0        
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
 
