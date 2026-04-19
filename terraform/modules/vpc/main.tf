3ac16df5b2d7614539cb76591279c6a7ea609f4f
zenitugo <ugochiukaegbu19@gmail.com>
2026-04-19 11:04:53 +0100

    configuration to create public subnet 1

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,10 +1,25 @@
 # Create VPC
 
-resource "aws_vpc" "main" {
+resource "aws_vpc" "vpc" {
   cidr_block = var.vpc_cidr
   instance_tenancy = "default"
-  
+
   tags = {
     Name = "${var.project_name}-vpc"
+  }
+}
+
+
+# Create public subnet 1
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id = aws_vpc.vpc.id
+  cidr_block = var.subnet_cidr_public_1
+  availability_zone = "data.aws_availability_zones.available.names[0]"
+  map_public_ip_on_launch = true
+  dns_support = true
+  dns_hostnames = true
+
+  tags = {
+    Name = "${var.project_name}-public-subnet-1"
   }
 }
