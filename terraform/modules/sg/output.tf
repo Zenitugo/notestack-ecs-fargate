136cd71a51bab7d959d67c50d112a5ad5e6d2785
zenitugo <ugochiukaegbu19@gmail.com>
2026-04-19 13:15:13 +0100

    output resource for rds sg id declared

--- terraform/modules/sg/output.tf
+++ terraform/modules/sg/output.tf
@@ -14,3 +14,8 @@ output "ecs_sg_frontend_id" {
 output "ecs_sg_backend_id" {
   value = aws_security_group.ecs_sg_backend.id
 }
+
+# output rds security group ID
+output "rds_sg_id" {
+  value = aws_security_group.rds_sg.id
+}   
