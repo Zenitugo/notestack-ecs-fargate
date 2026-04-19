3a80c2b9a7d6748c1a0c6cdae39a17a60786c66f
zenitugo <ugochiukaegbu19@gmail.com>
2026-04-19 23:51:52 +0100

    configuration to create repo to store the backend docker image

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -1,7 +1,18 @@
+# This resource creates an ECR repository for the frontend application.
 resource "aws_ecr_repository" "frontend_repository" {
   name = "${var.project_name}-repository"
 
   image_scanning_configuration {
     scan_on_push = true
   }
+}
+
+
+# This resource creates an ECR repository for the backend application.
+resource "aws_ecr_repository" "backend_repository" {
+  name = "${var.project_name}-backend-repository"       
+
+    image_scanning_configuration {
+        scan_on_push = true
+    }
 }
