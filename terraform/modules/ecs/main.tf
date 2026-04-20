aab5d342a1a8213af39a686bb54719b6daad5661
zenitugo <ugochiukaegbu19@gmail.com>
2026-04-20 14:15:20 +0100

    arranged the container definition json code properly

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -12,12 +12,13 @@ resource "aws_ecs_task_definition" "frontend_task" {
     {
     name           = "${var.project_name}-frontend-container",
     image          = "${var.frontend_repository_url}:latest",
-    container_port = var.frontend_port
-    host_port      = var.frontend_port
-    protocol        = "tcp"
-    aws_region     = var.region
-  },
-  {
+    portMappings = [
+      {
+        containerPort = var.frontend_port
+        hostPort      = var.frontend_port
+        protocol      = "tcp"
+      }
+    ]
     logConfiguration = {
       logDriver = "awslogs"
       options = {
@@ -35,6 +36,7 @@ resource "aws_ecs_task_definition" "frontend_task" {
   cpu                      = "256"
   memory                   = "512"
 
+
   depends_on = [aws_ecs_cluster.notestack,]
 }  
 
@@ -46,12 +48,13 @@ resource "aws_ecs_task_definition" "backend_task" {
     {
     name           = "${var.project_name}-backend-container",
     image          = "${var.backend_repository_url}:latest",
-    container_port = var.backend_port
-    host_port      = var.backend_port 
-    protocol        = "tcp"
-    aws_region     = var.region
-  },
-  {
+    portMappings = [
+      {
+        containerPort = var.backend_port
+        hostPort      = var.backend_port
+        protocol      = "tcp"
+      }
+    ]
     logConfiguration = {
       logDriver = "awslogs" 
       options = {
@@ -60,26 +63,30 @@ resource "aws_ecs_task_definition" "backend_task" {
         "awslogs-stream-prefix" = "ecs"
         }
     }
-  }
-  ])
-  secrets = [
-    {
-      name      = "DB_PASSWORD"
-      valueFrom = "${var.secrets_arn}:DB_PASSWORD::"
-    },
-    {
-      name      = "DB_USERNAME"
-      valueFrom = "${var.secrets_arn}:DB_USERNAME::"
-    },
-    {
-      name      = "DB_HOST"
-      valueFrom = "${var.secrets_arn}:DB_HOST::"
-    },
-    {
-      name      = "DB_PORT"
-      valueFrom = "${var.db_port}"
+    
+    secrets = [
+        {
+        name      = "DB_PASSWORD"
+        valueFrom = "${var.secrets_arn}:DB_PASSWORD::"
+        },
+        {
+        name      = "DB_USERNAME"
+        valueFrom = "${var.secrets_arn}:DB_USERNAME::"
+        },
+        {
+        name      = "DB_HOST"
+        valueFrom = "${var.secrets_arn}:DB_HOST::"
+        }
+    ]
+
+    environment = [
+      {
+        name  = "DB_PORT"
+        value = var.db_port
+      }
+    ]
     }
-  ]
+])
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
   execution_role_arn       = var.execution_role_arn
