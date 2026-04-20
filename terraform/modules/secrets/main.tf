8fdcaf999088f66f166e0eb4f53f115756a40c8b
zenitugo <ugochiukaegbu19@gmail.com>
2026-04-20 13:57:51 +0100

    remove the variables i didnt consider as sensitive

--- terraform/modules/secrets/main.tf
+++ terraform/modules/secrets/main.tf
@@ -6,8 +6,6 @@ resource "aws_secretsmanager_secret_version" "notestack_secret_version" {
   secret_id     = aws_secretsmanager_secret.notestack_secret.id
   secret_string = jsonencode({
   DB_HOST     = var.rds_endpoint
-  DB_PORT     = var.db_port
-  DB_NAME     = "${var.project_name}_db"
   DB_USER     = var.db_username
   DB_PASSWORD = var.db_password
 
