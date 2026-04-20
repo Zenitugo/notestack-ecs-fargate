3ed120df08ea35d956c07684334b0c39bcd76338
zenitugo <ugochiukaegbu19@gmail.com>
2026-04-20 20:12:49 +0100

    configuration to create alb to allow frontend to speak to backend

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -37,18 +37,18 @@ resource "aws_lb_listener" "http" {
 }
 
 
-# Create an Application Load Balancer for the frontend service
-resource "aws_lb" "notestack_frontend_lb" {
-  name               = "${var.project_name}-frontend-alb"
+# Create an Application Load Balancer for the backend service
+resource "aws_lb" "notestack_backend_lb" {
+  name               = "${var.project_name}-backend-alb"
   internal           = true
   load_balancer_type = "application"
-  security_groups    = [var.ecs_sg_frontend_id]
-  subnets            = [var.public_subnet_1_id, var.public_subnet_2_id]
+  security_groups    = [var.ecs_sg_backend_id]
+  subnets            = [var.private_subnet_1_id, var.private_subnet_2_id]
 }
 
-resource aws_lb_target_group "notestack_frontend_tg" {
-  name     = "${var.project_name}-frontend-tg"
-  port     = var.frontend_port
+resource aws_lb_target_group "notestack_backend_tg" {
+  name     = "${var.project_name}-backend-tg"
+  port     = var.backend_port
   protocol = "HTTP"
   vpc_id   = var.vpc_id
   target_type = "ip"
@@ -61,13 +61,13 @@ resource aws_lb_target_group "notestack_frontend_tg" {
     }
 }
 
-resource "aws_lb_listener" "frontend_http" {
-  load_balancer_arn = aws_lb.notestack_frontend_lb.arn
-  port              = var.frontend_port
+resource "aws_lb_listener" "backend_http" {
+  load_balancer_arn = aws_lb.notestack_backend_lb.arn
+  port              = var.backend_port
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.notestack_frontend_tg.arn
+    target_group_arn = aws_lb_target_group.notestack_backend_tg.arn
   }
 }
