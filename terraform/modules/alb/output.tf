b028af5c562e87627a3415ec50adc15cd02f82a9
zenitugo <ugochiukaegbu19@gmail.com>
2026-04-20 20:14:21 +0100

    output backend alb dns name

--- terraform/modules/alb/output.tf
+++ terraform/modules/alb/output.tf
@@ -2,3 +2,9 @@
 output "target_group_arn" {
   value = aws_lb_target_group.notestack_tg.arn
 }
+
+
+# Output backend ALB DNS name
+output "backend_URL" {
+  value = aws_lb.notestack_backend_lb.dns_name
+}
