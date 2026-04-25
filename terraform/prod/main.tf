7e8f0e4b40abb777d315b28dd8b85558a1156df2
zenitugo <ugochiukaegbu19@gmail.com>
2026-04-25 19:26:34 +0100

    changed modules to module

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -1,4 +1,4 @@
-modules "vpc"     {
+module "vpc"     {
     source                                    = "../modules/vpc"
     project_name                              = var.project_name
     vpc_cidr                                  = var.vpc_cidr
