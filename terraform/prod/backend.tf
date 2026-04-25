91fe6042e3a317be1d48e1ef7ba97ee778e573ec
zenitugo <ugochiukaegbu19@gmail.com>
2026-04-25 19:39:25 +0100

    changed bucket name

--- terraform/prod/backend.tf
+++ terraform/prod/backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" { 
-        bucket = "ugochi-project1-state"
+        bucket = "ugochi-project1-state-euc1"
         key     = "note-stack-app/ecs/terraform.tfstate"
         dynamodb_table = "ugochi-notestack-lock"
         region = "eu-central-1"
