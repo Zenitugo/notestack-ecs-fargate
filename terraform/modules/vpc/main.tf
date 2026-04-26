f010cd6434d8d4ff8e1008f78d50ac4cf01a2f4a
zenitugo <ugochiukaegbu19@gmail.com>
2026-04-26 15:28:24 +0100

    removed the quoutation marks from the az data resource

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -16,7 +16,7 @@ resource "aws_vpc" "vpc" {
 resource "aws_subnet" "public_subnet_1" {
   vpc_id = aws_vpc.vpc.id
   cidr_block = var.subnet_cidr_public_1
-  availability_zone = "data.aws_availability_zones.available.names[0]"
+  availability_zone = data.aws_availability_zones.available.names[0]
   map_public_ip_on_launch = true
   tags = {
     Name = "${var.project_name}-public-subnet-1"
@@ -28,7 +28,7 @@ resource "aws_subnet" "public_subnet_1" {
 resource "aws_subnet" "public_subnet_2" {
   vpc_id = aws_vpc.vpc.id
   cidr_block = var.subnet_cidr_public_2
-  availability_zone = "data.aws_availability_zones.available.names[1]"
+  availability_zone = data.aws_availability_zones.available.names[1]
   map_public_ip_on_launch = true
 
   tags = {
@@ -41,7 +41,7 @@ resource "aws_subnet" "public_subnet_2" {
 resource "aws_subnet" "private_subnet_1" {
     vpc_id = aws_vpc.vpc.id
     cidr_block = var.subnet_cidr_private_1
-    availability_zone = "data.aws_availability_zones.available.names[0]"
+    availability_zone = data.aws_availability_zones.available.names[0]
     map_public_ip_on_launch = false
 
     tags = {
@@ -54,7 +54,7 @@ resource "aws_subnet" "private_subnet_1" {
 resource "aws_subnet" "private_subnet_2" {
     vpc_id = aws_vpc.vpc.id     
     cidr_block = var.subnet_cidr_private_2
-    availability_zone = "data.aws_availability_zones.available.names[1]"
+    availability_zone = data.aws_availability_zones.available.names[1]
     map_public_ip_on_launch = false
     tags = {
         Name = "${var.project_name}-private-subnet-2"
@@ -65,7 +65,7 @@ resource "aws_subnet" "private_subnet_2" {
 resource "aws_subnet" "private_subnet_3" {
     vpc_id = aws_vpc.vpc.id     
     cidr_block = var.subnet_cidr_private_3
-    availability_zone = "data.aws_availability_zones.available.names[2]"
+    availability_zone = data.aws_availability_zones.available.names[2]
     map_public_ip_on_launch = false
     tags = {
         Name = "${var.project_name}-private-subnet-3"
@@ -76,7 +76,7 @@ resource "aws_subnet" "private_subnet_3" {
 resource "aws_subnet" "private_subnet_4" {
     vpc_id = aws_vpc.vpc.id     
     cidr_block = var.subnet_cidr_private_4
-    availability_zone = "data.aws_availability_zones.available.names[3]"  
+    availability_zone = data.aws_availability_zones.available.names[3]  
     map_public_ip_on_launch = false
     tags = {
         Name = "${var.project_name}-private-subnet-4"
