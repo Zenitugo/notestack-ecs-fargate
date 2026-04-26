93ed3ec2bd155559dc62ac29a78a0d5deba7c79d
zenitugo <ugochiukaegbu19@gmail.com>
2026-04-26 18:54:48 +0100

    outputs needed by github

--- terraform/prod/output.tf
+++ terraform/prod/output.tf
@@ -1,23 +1,23 @@
 # Output Cluster name
 output "cluster_name" {
-  value = aws_ecs_cluster.notestack.name
+  value = module.ecs.cluster_name
 }
 
 # Output backend service name
 output "backend_service_name" {
-  value = aws_ecs_service.backend_service.name
+  value = module.ecs.backend_service_name
 }
 
 # Output frontend service name
 output "frontend_service_name" {
-  value = aws_ecs_service.frontend_service.name
+  value = module.ecs.frontend_service_name
 }
 
 # Output values for the ECR repositories
 output "frontend_repository_url" {
-  value = aws_ecr_repository.frontend_repository.repository_url
+  value = module.ecr.frontend_repository_url
 }
 
 output "backend_repository_url" {
-  value = aws_ecr_repository.backend_repository.repository_url
+  value = module.ecr.backend_repository_url
 }
