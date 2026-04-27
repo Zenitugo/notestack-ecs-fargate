413ce0436d10418d11c318bf5c0099934176d081
zenitugo <ugochiukaegbu19@gmail.com>
2026-04-27 08:47:26 +0100

    removed backend target arn since it no longer exist

--- terraform/modules/alb/output.tf
+++ terraform/modules/alb/output.tf
@@ -2,9 +2,3 @@
 output "target_group_arn" {
   value = aws_lb_target_group.notestack_tg.arn
 }
-
-
-# Output backend target group arn
-output "backend_target_group_arn" {
-  value = aws_lb_target_group.notestack_backend_tg.arn
-}
