a856bcb47493d245846e2cd654c7fb08d4de0a59
zenitugo <ugochiukaegbu19@gmail.com>
2026-04-27 10:11:46 +0100

    changed user to username

--- note-stack-app/backend/main.py
+++ note-stack-app/backend/main.py
@@ -23,7 +23,7 @@ def get_db():
         host=os.getenv("DB_HOST"),
         port=os.getenv("DB_PORT"),
         database=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
+        user=os.getenv("DB_USERNAME"),
         password=os.getenv("DB_PASSWORD"),
     )
     return conn
