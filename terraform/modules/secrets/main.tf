db3c457ee36eb23a5d61c5e3140af3ffeae50623
zenitugo <ugochiukaegbu19@gmail.com>
2026-05-06 06:04:55 +0100

    gave secret a new name cause old name is yet to be deleted

--- terraform/modules/secrets/main.tf
+++ terraform/modules/secrets/main.tf
@@ -1,5 +1,5 @@
 resource "aws_secretsmanager_secret" "notestack_secret" {
-  name = "${var.project_name}-new-secret"
+  name = "${var.project_name}-new-secret-2"
 }
 
 resource "aws_secretsmanager_secret_version" "notestack_secret_version" {
